4a24474aebeeac7679e9a569de50bdb5a1b67fdb
nacoraco <40896702+nacoraco@users.noreply.github.com>
2022-12-05 19:03:06 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -39,4 +39,4 @@ echo \
   
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
